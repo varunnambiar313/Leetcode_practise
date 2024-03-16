2ac74321af76c9f09fc9a3472918a45597557fc0
Varun Nambiar <45871257+varunnambiar313@users.noreply.github.com>
2024-03-17 03:45:28 +0530

    Update squares_of_a_sorted_array.py

--- Arrays/squares_of_a_sorted_array.py
+++ Arrays/squares_of_a_sorted_array.py
@@ -1,15 +1,23 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        poppers=[]
-        for i in range(len(nums)):
-            if nums[i]<0:
-                for j,k in reversed(list(enumerate(nums))):
-                    if abs(nums[i])>nums[j]:
-                        nums.insert(j+1,abs(nums[i]))
-                        poppers.append(int(i))
-                        break
-        for p in range(len(poppers)):
-            nums.pop(0)
-        for o in range(len(nums)):
-            nums[o] = nums[o]**2
-        return nums
+        result = []
+        i = 0
+        j = len(nums)-1
+        while i!=j:
+            if i>j:
+                break
+            if abs(nums[i])>abs(nums[j]):
+                result = [nums[i]**2]+result
+                i+=1
+            elif abs(nums[i])<abs(nums[j]):
+                result = [nums[j]**2]+result
+                j-=1
+            else:
+                result = [nums[j]**2]+result
+                result = [nums[i]**2]+result
+                i+=1
+                j-=1
+        if i==j:
+            result = [nums[i]**2]+result
+        return result
+                
